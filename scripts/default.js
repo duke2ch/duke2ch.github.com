89bc89ccb780a880af892142c5276891cef76e3a
Yu I <lagfer@youck.org>
2011-04-30 01:23:38 +0900

    メニューバー調整、目次生成スクリプト追加、Twitter調整、背景追加...!

--- scripts/default.js
+++ scripts/default.js
@@ -1,5 +1,3 @@
-// TODO: TOC 自動生成やる。
-
 $(function() {
     var URL = 'http://search.twitter.com/search.json?callback=?',
         tweets = [];
@@ -42,11 +40,10 @@ $(function() {
                 tweets[i].id_str + '"><strong>@' +
                 tweets[i].from_user + '</strong></a>: ' +
                 text + '</li>');
-            $('#twitter').fadeIn(400).css('display', 'block');
+            $('#twitter').slideDown(400).css('display', 'block');
         }
     }
 
-    $('header').attr('id', 'babe-' + Math.floor(Math.random() * 2));
     getSearchResults(URL,
         {'from': '3drealms', 'q': 'Duke OR Nukem OR DNF'},
         function() {
@@ -69,4 +66,62 @@ $(function() {
             );
         }
     );
+
+    $('header').attr('id', 'babe-' + Math.floor(Math.random() * 2));
+
+    $("#toc").bind("click", function () {
+        /**
+         * TOC generator
+         * @see http://www.mikage.to/jquery/js/jquery_site.js
+         */
+        var assigned_level = 0,
+            current_level = 0,
+            id_number = 1,
+            parent_node = "article",
+            toc_html = "";
+
+        $(parent_node).children().each(function () {
+            if (this.nodeName.length === 2 && this.nodeName.charAt(0) === "H") {
+                $(this).attr("class", "heading");
+            }
+        });
+
+        $(".heading").each(function () {
+            current_level = this.nodeName.charAt(1);
+
+            $(this).attr("id", "toc-" + id_number);
+
+            // Close a list if a same level list follows.
+            if (assigned_level !== current_level - 1) {
+                toc_html += "</li>"
+            }
+
+            // Open parent lists if a child list follows.
+            while (assigned_level < current_level) {
+                toc_html += "<ol>";
+                assigned_level += 1;
+            }
+
+            // Close child lists and the parent list if
+            // the same level parent list follows.
+            while (assigned_level > current_level) {
+                toc_html += "</ol></li>";
+                assigned_level -= 1;
+            }
+
+            toc_html += 
+                '<li><a href="#' + this.id + '">' + $(this).text() + "</a>";
+            id_number += 1;
+        });
+
+        // Close everything
+        while (assigned_level > 0) {
+            toc_html += "</li></ol>";
+            assigned_level -= 1;
+        }
+
+        $(".heading").append('<a href="#top" class="toc">△</a>');
+        $("#toc").unbind("click");
+        $("#toc").append(toc_html);
+    });
 });
